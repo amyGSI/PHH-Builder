c40b87aedc459287c76e6584459faa324dc5a5cd
Amy Grace <00p513-dev@protonmail.com>
2020-07-29 17:45:55 +0100

    .github

---
+++ .github/workflows/robot.py
@@ -0,0 +1,34 @@
+# Thanks to github.com/usernein
+
+import os
+import pyrogram
+
+with open('Aonly.txt') as f:
+    os.environ['CAT_FILE'] = f.read()
+
+rom = os.getenv('ROM_NAME')
+zip = os.getenv('ZIP_NAME')
+cat = os.getenv('CAT_FILE')
+romurl = os.getenv('ROM_URL')
+da = os.getenv('DOWNLOAD_A')
+dab = os.getenv('DOWNLOAD_AB')
+
+with pyrogram.Client('bot', os.getenv('API_ID'), os.getenv('API_HASH'), bot_token=os.getenv('BOT_TOKEN')) as client:
+    client.send_message(
+        text=f"""<b>{zip} GSI For ARM 32 Binder 64 A/AB Devices</b>
+
+<b>Firmware Base:</b> <a href="{romurl}">HERE</a>
+
+<b>Information:</b>
+<code>{cat}</code>
+
+<b>Download A-Only:</b> <a href="{da}">HERE</a>
+<b>Download AB:</b> <a href="{dab}">HERE</a>
+
+<b>amyGSI</b> - <i>Channel</i>: @amyGSI
+
+<a href="https://github.com/HitaloSama/ErfanGSIs">Ported using my fork of Hitsuki's ErfanGSIs Edit</a>""",
+        chat_id=os.getenv('CHAT_ID'),
+        parse_mode="html",
+        disable_web_page_preview=True
+    )
